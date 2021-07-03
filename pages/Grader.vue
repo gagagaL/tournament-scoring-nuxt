04b0ac72e781562e4f0ee157782f85383037faae
masaaki-kawaguchi <masaaki.kawaguchi@fox-hound.jp>
2021-07-03 23:14:38 +0900

    モーダル追加

--- pages/Grader.vue
+++ pages/Grader.vue
@@ -41,7 +41,6 @@
             to="/grader"
           >Reset</v-btn>
       </p>
-      <hr />
         <p>
         <v-btn
           color="danger"
@@ -49,11 +48,55 @@
           block
           elevation="20"
           to="/grader"
+          @click.stop="dialog = true"
         >OK</v-btn>
         </p>
         <v-card-actions>
         </v-card-actions>
       </v-card>
+      <v-dialog
+        v-model="dialog"
+        max-width="290"
+      >
+        <v-card>
+          <v-card-title class="text-h5">
+            Use Google's location service?
+          </v-card-title>
+
+          <v-card-text>
+            Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn
+              color="green darken-1"
+              text
+              @click="dialog = false"
+            >
+              Disagree
+            </v-btn>
+
+            <v-btn
+              color="green darken-1"
+              text
+              @click="dialog = false"
+            >
+              Agree
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-col>
   </v-row>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      dialog: false,
+    }
+  },
+}
+</script>
